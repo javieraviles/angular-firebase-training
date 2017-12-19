c613562261e2dce0c487bc253cb0701b4d8c5c67
Javier Aviles <avileslopez.javier@gmail.com>
2017-12-19 13:13:12 +0100

    Mock a list of ToDOs and list them

---
+++ src/app/mock-todos.ts
@@ -0,0 +1,9 @@
+import { Todo } from './todo';
+
+export const TODOS: Todo[] = [
+  { id: 1, title: 'Finish the report', completed: false },
+  { id: 2, title: 'Write the proposal', completed: true },
+  { id: 3, title: 'Tell the boss to raise my salary', completed: false },
+  { id: 4, title: 'Meet John', completed: true },
+  { id: 5, title: 'Play Squash with Susan', completed: false }
+];
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,12 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 import {Todo } from '../todo';
 
+import { TODOS } from '../mock-todos';
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+
+  todos = TODOS;
 
   constructor() { }
 
